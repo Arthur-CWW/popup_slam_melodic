2b4052427ea3d48acabc489b354ceb336e69bbaa
chun1 <arthur@chunai.dev>
2024-02-18 13:02:45 +1100

    change install script to remove deprecated feature

--- install_dependenices.sh
+++ install_dependenices.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # compile bag of words
-cd pop_planar_slam/Thirdparty/DBoW2
+# cd pop_planar_slam/Thirdparty/DBoW2
+# replace with pushd and popd
+pushd pop_planar_slam/Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j2
 
 # install isam dependency
-sudo apt-get install cmake libsuitesparse-dev libeigen3-dev libsdl1.2-dev doxygen graphviz
-
-# install python library
-sudo easy_install pip
-cd ../../../..
-sudo pip install -r py_requirements.txt
+sudo apt-get install cmake libsuitesparse-dev libeigen3-dev libsdl1.2-dev doxygen graphviz -y
+popd
+# sudo pip install -r py_requirements.txt
+python -m venv venv
+source venv/bin/activate
+pip install -r py_requirements.txt
